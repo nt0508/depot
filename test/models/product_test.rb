01447cb90b8b0c118292036374b4a73f1609843b
nt0508 <i0tl.96er@gmail.com>
2017-11-15 14:41:56 +0700

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  def new_product(image_url)
+    Product.new(title:          "My Book Title",
+                description:    "yyy",
+                price:          1,
+                image_url:      image_url)
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:          products(:ruby).title,
+                          description:    "yyy",
+                          price:          1,
+                          image_url:      "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                  product.errors[:title]
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+                            http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
 end

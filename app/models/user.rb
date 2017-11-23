0200b978f39bf4085a50339e10396e225fc9b8bd
nevergiveup2212 <blackcat22121996@gmail.com>
2017-11-23 10:03:48 +0700

    Finish J1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_secure_password
   after_destroy :ensure_an_admin_remains
-class Error < StandardError
-end
-private
-def ensure_an_admin_remains
-if User.count.zero?
-raise Error.new "Can't delete last user"
-end
-end
+  class Error < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    raise Error, "Can't delete last user" if User.count.zero?
+  end
 end
